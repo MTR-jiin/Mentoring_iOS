4ed7c6bd45b305475b3ac3eae3b46aaa9fcf303e
kimjiin <kj980926@naver.com>
2022-05-05 02:07:03 +0900

    Setting keyBoard & Custom textField underLine

--- JobPlanetMyPage/MyPage/MyPageSettingViewController.swift
+++ JobPlanetMyPage/MyPage/MyPageSettingViewController.swift
@@ -8,24 +8,55 @@
 import UIKit
 
 class MyPageSettingViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var birthTextField: UITextField!
+    @IBOutlet weak var careerTextField: UITextField!
+    @IBOutlet weak var nowJobTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBarController?.tabBar.isHidden = true
+        configureUI()
+        nameTextField.becomeFirstResponder()
+        hideKeyboardWhenTappedBackground()
     }
     
+    private func configureUI() {
+        [nameTextField, emailTextField, phoneNumberTextField, genderTextField, birthTextField, careerTextField, nowJobTextField].forEach {
+            $0?.makeUnderLine()
+        }
+    }
+
     @IBAction func tapBackBtn(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//텍스트 필드 밑줄 생성
+extension UITextField {
+    func makeUnderLine() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: self.frame.height - 1, width: self.frame.width - 25, height: 1.0)
+        bottomLine.backgroundColor = UIColor.systemGray5.cgColor
+        borderStyle = UITextField.BorderStyle.none
+        layer.addSublayer(bottomLine)
     }
-    */
+}
 
+//빈 화면 터치시 키보드 내림
+extension UIViewController {
+    func hideKeyboardWhenTappedBackground() {
+         let tapEvent = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+         tapEvent.cancelsTouchesInView = false
+         view.addGestureRecognizer(tapEvent)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
