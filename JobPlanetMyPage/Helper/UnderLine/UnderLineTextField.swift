5edb1dc73467935f782ede9d9e52721bd83bea2b
kimjiin <kj980926@naver.com>
2022-05-12 13:32:14 +0900

    커스텀 뷰 스택에 구성

--- JobPlanetMyPage/Helper/UnderLine/UnderLineTextField.swift
+++ JobPlanetMyPage/Helper/UnderLine/UnderLineTextField.swift
@@ -8,6 +8,10 @@
 import Foundation
 import UIKit
 
+enum LineColor {
+    case gray, green
+}
+
 class UnderLineTextField: UIView {
     
     @IBOutlet private weak var titleLabel: UILabel!
@@ -18,10 +22,19 @@ class UnderLineTextField: UIView {
         super.init(frame: frame)
         loadView()
     }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadView()
     }
+    
+//    convenience init(frame: CGRect, title: String, placeHolder: String, underLine: LineColor) {
+//        self.init(frame: frame)
+//        self.titleLabel.text = title
+//        self.textField.placeholder = placeHolder
+//        self.lineView.tintColor = underLine == .gray ? .systemGray5 : UIColor(named: "lightGreen")
+//    }
+//
     private func loadView() {
         guard let xibName = NSStringFromClass(self.classForCoder).components(separatedBy: ".").last,
               let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as? UIView else { return }
@@ -34,6 +47,12 @@ class UnderLineTextField: UIView {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: nib, bundle: bundle)
         return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
+    func configure(infoData: textFieldInfo) {
+        self.titleLabel.text = infoData.title
+        self.textField.placeholder = infoData.placeHolder
+        self.lineView.tintColor = infoData.underLine == .gray ? .systemGray5 : UIColor(named: "lightGreen")
     }
         
 }
