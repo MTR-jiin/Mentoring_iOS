95a2a5696effa44224894274e8f885cf56269606
kimjiin <kj980926@naver.com>
2022-05-19 23:30:33 +0900

    버튼 탭할시 제안 상태 보임

--- JobPlanetMyPage/Offer/OfferDetailViewController.swift
+++ JobPlanetMyPage/Offer/OfferDetailViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class OfferDetailViewController: UIViewController {
 
+    @IBOutlet weak var offerButtonsStackView: UIStackView!
+    @IBOutlet weak var offerDescriptionLabel: UILabel!
+    @IBOutlet weak var offerButtonsView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        makeViewShadow()
+        
+    }
+    private func makeViewShadow() {
+        offerButtonsView.layer.shadowOpacity = 0.2
+        offerButtonsView.layer.shadowOffset = CGSize(width:  0, height: -5)
+        offerButtonsView.layer.shadowRadius = 5
+        offerButtonsView.layer.shadowColor = UIColor.gray.cgColor
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tappedAcceptOrDenyBtn(_ sender: UIButton) {
+        offerButtonsStackView.isHidden = true
+        if sender.titleLabel?.text == "거절" {
+            offerDescriptionLabel.tintColor = .gray
+            offerDescriptionLabel.text = "거절한 제안입니다."
+        } else {
+            offerDescriptionLabel.tintColor = UIColor(named: "lightGreen")
+            offerDescriptionLabel.text = "수락한 제안입니다."
+        }
     }
-    */
-
+    
+    @IBAction func tappedBackBtn(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
 }
