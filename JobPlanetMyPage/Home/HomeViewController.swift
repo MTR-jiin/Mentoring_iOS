6f45d16a371881c02bcfa2dee158c0af4320b86a
kimjiin <kj980926@naver.com>
2022-05-26 13:21:09 +0900

    급상승 검색기업 folding 개발중
    tableview 높이 동적으로 수정해야함,,

--- JobPlanetMyPage/Home/HomeViewController.swift
+++ JobPlanetMyPage/Home/HomeViewController.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-    
+struct SearchCompany {
+    let header: String
+    var items: [Item]
+    var isOpen = false
 
+    struct Item {
+        let ranking: Int
+        let company_id: Int
+        let company_name: String
+    }
+}
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    private var dummy: SearchCompany = SearchCompany(header: "실시간 어쩌고", items: [
+                                    SearchCompany.Item(ranking: 1, company_id: 2123, company_name: "카카오"),
+                                    SearchCompany.Item(ranking: 2, company_id: 3232, company_name: "네이버"),
+                                    SearchCompany.Item(ranking: 3, company_id: 21544523, company_name: "토스")])
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        customSearchBar()
-        // Do any additional setup after loading the view.
+        makeSearchBar()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-    private func customSearchBar() {
+    private func makeSearchBar() {
         let searchBar = UISearchBar()
         searchBar.placeholder = "기업, 채용공고 검색"
         searchBar.setImage(UIImage(named: "logo"), for: UISearchBar.Icon.search, state: .normal)
@@ -24,4 +45,48 @@ class HomeViewController: UIViewController {
         self.navigationItem.titleView = searchBar
     }
 
+    
+
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if dummy.isOpen { //섹션이 열려 있으면
+            return dummy.items.count + 1
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeue(type: SearchHeaderTableViewCell.self, for: indexPath) else {
+                return .init()
+            }
+            cell.foldingState = dummy.isOpen
+            return cell
+        } else {
+            guard let cell = tableView.dequeue(type: SearchCompanyTableViewCell.self, for: indexPath) else {
+                return .init()
+            }
+            cell.data = dummy.items[indexPath.row-1]
+            return cell
+        }
+    }
+}
+
+
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        dummy.isOpen = !dummy.isOpen
+        print(dummy.isOpen)
+        tableView.reloadSections([indexPath.section], with: UITableView.RowAnimation.automatic)
+        
+    }
 }
