9c72fc45cb737d1a46ab7736f38774932087d9d3
kimjiin <kj980926@naver.com>
2022-06-26 16:06:49 +0900

    MainHome TableView Rx로 변경
    수정중,,

--- JobPlanetMyPage/Home/SearchHomeViewController.swift
+++ JobPlanetMyPage/Home/SearchHomeViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 import RxSwift
 import RxCocoa
 import InfiniteLayout
+import RxDataSources
+
+struct Section {
+    var items: [Item]
+}
+
+enum Item {
+    case headerCell
+    case defaultCell
+}
+
+extension Section: SectionModelType {
+    
+    init(original: Section, items: [Item] = []) {
+        self = original
+        self.items = items
+    }
+}
 
 class SearchHomeViewController: UIViewController {
     
@@ -18,15 +36,49 @@ class SearchHomeViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
     private let rankViewModel: SearchHomeRankViewModel = .init()
-    private let viewModel = HeadLineViewModel()
+    private let viewModel: HeadLineViewModel = .init()
     private let disposeBag: DisposeBag = .init()
+    //RxTableView
+    private let data = BehaviorRelay<[Section]>(value: [])
+    private lazy var dataSource = RxTableViewSectionedReloadDataSource<Section> { [weak self] dataSource, tableview, indexPath, item in
+            switch item {
+            case .headerCell:
+                guard let cell = tableview.dequeueReusableCell(withIdentifier: "HeadLineViewCell", for: indexPath) as? HeadLineViewCell else { return UITableViewCell() }
+                //🚨저장 프로퍼티에서 저장프로퍼티 접근하는게 이상한데,, 방법을 모르겠네요 ㅜㅜ
+                //1
+//                viewModel.headModelData
+//                        .bind(to: cell.collectionView.rx.items(infinite: true)) {
+//                            collectionView, row, element in
+//                            let indexPath = IndexPath(row: row, section: 0)
+//                            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InfinityCollectionViewCell", for: indexPath) as? InfinityCollectionViewCell else { return UICollectionViewCell() }
+//                            cell.configure(element)
+//                            return cell
+//                        }.disposed(by: disposeBag)
+                
+                //2
+//                cell.collectionView.rx.modelSelected(HeadLineModel.self)
+//                    .asDriver()
+//                     .drive(onNext: { current in
+//                         print(current)
+//                         cell.configureHeadData(current)
+//                     }).disposed(by: disposeBag)
+                return cell
+            case .defaultCell:
+                let cell = tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                return cell
+            }
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
         headerView.delegate = self //delegate
         rankDataBinding()
         settingTableView()
+       
     }
+    
     private func rankDataBinding() {
         rankViewModel.rankModelData
             .bind(to: headerView.tableView.rx.items(cellType: SearchHomeHeaderRankCell.self)){ idx, item, cell in
@@ -34,13 +86,17 @@ class SearchHomeViewController: UIViewController {
             }
             .disposed(by: disposeBag)
     }
-
-    
+        
     private func settingTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
         let nib = UINib(nibName: "HeadLineViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "HeadLineViewCell")
+        
+        let item = Item.headerCell
+        let item2 = Item.defaultCell
+        let section = Section(items: [item, item2, item2])
+        data.bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        data.accept([section])
     }
     
 }
@@ -64,56 +120,12 @@ extension SearchHomeViewController {
     }
 }
 
-//MARK: - HeadLine
-
-extension SearchHomeViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            guard let cell = tableView.dequeue(type: HeadLineViewCell.self, for: indexPath) else {
-                return .init()
-            }
-            //infinity Cell Binding
-            viewModel.headModelData
-                .bind(to: cell.collectionView.rx.items(infinite: true)) {
-                    collectionView, row, element in
-                    let indexPath = IndexPath(row: row, section: 0)
-                    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InfinityCollectionViewCell", for: indexPath) as? InfinityCollectionViewCell else { return UICollectionViewCell() }
-                    cell.configure(element)
-                    return cell
-                }.disposed(by: disposeBag)
-            //HeadLine Binding
-            // 🚨 여기서 current 값이 없음 ..
-            cell.collectionView.rx.modelSelected(HeadLineModel.self)
-                .asDriver()
-                 .drive(onNext: { current in
-                     print(current)
-                     cell.configureHeadData(current)
-                 }).disposed(by: disposeBag)
-            return cell
-        }
-        else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            cell.textLabel?.text = "dsds"
-            return cell
-        }
-    }
-
-}
-
 extension SearchHomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 250
-        }
-        return 40
+        if indexPath.row == 0 { return 250 }
+        else { return 40 }
     }
 }
-
 
 extension Reactive where Base: UITableView {
     public func items<Sequence: Swift.Sequence, Cell: UITableViewCell, Source: ObservableType>
